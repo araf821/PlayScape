35858b50f43917aac62aeeb12e33c99d01a87e2b
araf821 <araf.ahmed200@gmail.com>
2023-08-12 23:28:54 -0400

    initialized navbar and overall body layout + fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,42 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Comfortaa, Pacifico } from "next/font/google";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "PixelLand",
+  description: "A Reddit clone, but better.",
+};
+
+export const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "antialias light bg-white text-slate-900",
+        pacifico.className,
+        comfortaa.className,
+      )}
+    >
+      <body className="min-h-screen bg-slate-50 pt-12 antialiased">
+        <Navbar />
+        <div className="container mx-auto h-full max-w-7xl pt-12">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }

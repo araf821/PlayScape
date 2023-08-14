60f44bd124c703b8b5e7afbb7abe9595994787a7
araf821 <araf.ahmed200@gmail.com>
2023-08-13 20:37:54 -0400

    implemented user authentication with oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import { FC } from "react";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
+const Navbar: FC<NavbarProps> = async ({}) => {
+  const session = await getAuthSession();
+
   return (
     <div className="fixed inset-x-0 top-0 z-10 h-fit border-b border-zinc-300 bg-zinc-100 py-2">
       <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-2">
@@ -18,9 +21,13 @@ const Navbar: FC<NavbarProps> = ({}) => {
 
         {/* Search bar will go here */}
 
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <div>Logged in as {session.user?.name}</div>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );

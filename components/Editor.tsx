08240b57ae6b657a1da02ae4dc57728b15dd7199
araf821 <araf.ahmed200@gmail.com>
2023-08-17 23:08:25 -0400

    finalized editor + added all the client side functionalities needed to publish a post

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -7,6 +7,10 @@ import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type EditorJS from "@editorjs/editorjs";
 import { uploadFiles } from "@/lib/uploadthing";
+import { toast } from "@/hooks/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { usePathname, useRouter } from "next/navigation";
 
 interface EditorProps {
   communityId: string;
@@ -26,14 +30,12 @@ const Editor: FC<EditorProps> = ({ communityId }) => {
     },
   });
 
-  const editorRef = useRef<EditorJS | null>(null);
-  const [isMounted, setIsMounted] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setIsMounted(true);
-    }
-  }, []);
+  const editorRef = useRef<EditorJS | null>(null);
+  const _titleRef = useRef<HTMLTextAreaElement | null>(null);
+  const [isMounted, setIsMounted] = useState(false);
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
@@ -60,7 +62,7 @@ const Editor: FC<EditorProps> = ({ communityId }) => {
           linkTool: {
             class: LinkTool,
             config: {
-              endPoint: "/api/link",
+              endpoint: "/api/link",
             },
           },
           image: {
@@ -98,26 +100,110 @@ const Editor: FC<EditorProps> = ({ communityId }) => {
   }, []);
 
   useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        value;
+        toast({
+          title: "Something went wrong.",
+          description: (value as { message: string }).message,
+          variant: "destructive",
+        });
+      }
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
     const init = async () => {
       await initializeEditor();
 
       setTimeout(() => {
-        // _titleRef?.current?.focus()
-      }, 10);
+        _titleRef?.current?.focus();
+      }, 0);
     };
 
     if (isMounted) {
       init();
 
-      return () => {};
+      return () => {
+        editorRef.current?.destroy();
+        editorRef.current = null;
+      };
     }
   }, [isMounted, initializeEditor]);
 
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      communityId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        title,
+        content,
+        communityId,
+      };
+
+      const { data } = await axios.post("/api/community/post/create", payload);
+
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: "Something went wrong.",
+        description: "Your post was not published, please try again later.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      const newPathname = pathname.split("/").slice(0, -1).join("/");
+      router.push(newPathname);
+
+      router.refresh();
+
+      return toast({
+        title: "Post published!",
+      });
+    },
+  });
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await editorRef.current?.save();
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      communityId,
+    };
+
+    createPost(payload);
+  }
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const { ref: titleRef, ...rest } = register("title");
+
   return (
     <div className="w-full rounded-lg border border-zinc-200 bg-zinc-50 p-4">
-      <form className="w-full" onSubmit={() => {}} id="community-post-form">
+      <form
+        className="w-full"
+        onSubmit={handleSubmit(onSubmit)}
+        id="community-post-form"
+      >
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+            ref={(e) => {
+              titleRef(e);
+              _titleRef.current = e;
+            }}
+            {...rest}
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
             placeholder="Title"
           />

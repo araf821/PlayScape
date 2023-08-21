2b1727625a33d903055a440a970f834ce545a23a
araf821 <araf.ahmed200@gmail.com>
2023-08-20 21:26:08 -0400

    initialized single post page + implemented post vote loader with suspense

--- app/community/[slug]/post/[postId]/page.tsx
+++ app/community/[slug]/post/[postId]/page.tsx
@@ -1,9 +1,87 @@
-import { FC } from "react";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
+import db from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { CachedPost } from "@/types/redis";
+import { Post, PostVote, User } from "@prisma/client";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+import { notFound } from "next/navigation";
+import { FC, Suspense } from "react";
 
-interface pageProps {}
+interface pageProps {
+  params: {
+    postId: string;
+  };
+}
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const page: FC<pageProps> = async ({ params }: pageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`,
+  )) as CachedPost;
+
+  let post: (Post & { votes: PostVote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) {
+    return notFound();
+  }
+
+  return (
+    <div className="">
+      <div className="flex h-full flex-col items-center justify-between sm:flex-row sm:items-start">
+        <Suspense fallback={<PostVoteLoader />}>
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
+
+function PostVoteLoader() {
+  return (
+    <div className="flex w-20 flex-col items-center pr-6">
+      {/* upvote btn */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* display of votes */}
+      <div className="py-2 text-center text-sm font-medium text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote btn */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 
 export default page;
---
+++ components/post-vote/PostVoteServer.tsx
@@ -0,0 +1,54 @@
+import { getAuthSession } from "@/lib/auth";
+import { Post, PostVote, VoteType } from "@prisma/client";
+import { notFound } from "next/navigation";
+import { FC } from "react";
+import PostVoteClient from "./PostVoteClient";
+
+interface PostVoteServerProps {
+  postId: string;
+  initialVotesAmount?: number;
+  initialVoteType?: VoteType;
+  getData?: () => Promise<(Post & { votes: PostVote[] }) | null>;
+}
+
+const PostVoteServer: FC<PostVoteServerProps> = async ({
+  postId,
+  initialVoteType,
+  initialVotesAmount,
+  getData,
+}) => {
+  const session = await getAuthSession();
+
+  let _numOfVotes: number = 0;
+  let _currentVoteType: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    if (!post) {
+      return notFound();
+    }
+
+    _numOfVotes = post.votes.reduce((acc, vote) => {
+      if (vote.type === "UP") return acc + 1;
+      if (vote.type === "DOWN") return acc - 1;
+      return acc;
+    }, 0);
+
+    _currentVoteType = post.votes.find(
+      (vote) => vote.userId === session?.user.id,
+    )?.type;
+  } else {
+    _numOfVotes = initialVotesAmount!;
+    _currentVoteType = initialVoteType;
+  }
+
+  return (
+    <PostVoteClient
+      initialVotesAmount={_numOfVotes}
+      initialVote={_currentVoteType}
+      postId={postId}
+    />
+  );
+};
+
+export default PostVoteServer;
